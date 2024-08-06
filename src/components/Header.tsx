f940e1e74d8c89b339cdfbc793c3f4f0593dac90
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-06 16:54:47 +0200

    Header: Fix links to all pages as a.href

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
 import { ReactElement } from "react";
 
 export function Header(): ReactElement {
-  return <div className="header">My todo list</div>;
+  return (
+    <div className="header">
+      <div className="">My todo list</div>
+      <nav className="navbar">
+        <a href="/">Home</a>
+        <a href="/add-todo">Add</a>
+        <a href="/about">About</a>
+      </nav>
+    </div>
+  );
 }
