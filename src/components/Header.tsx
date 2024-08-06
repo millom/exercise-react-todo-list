e8113b8094888932976bc05af0a57b708c1a2f46
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-06 17:05:01 +0200

    Header: Add links using React

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,24 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 export function Header(): ReactElement {
   return (
     <div className="header">
       <div className="">My todo list</div>
-      <nav className="navbar">
-        <a href="/">Home</a>
+      <div className="navbar">
+        {/* <a href="/">Home</a>
         <a href="/add-todo">Add</a>
-        <a href="/about">About</a>
-      </nav>
+        <a href="/about">About</a> */}
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/add-todo" className="link">
+          Add todo
+        </Link>
+        <Link to="/about" className="link">
+          About
+        </Link>
+      </div>
     </div>
   );
 }
