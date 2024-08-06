742f398c60cbe3926e52af5843cce2f2ca622cab
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-06 17:28:43 +0200

    AboutPage; Add todos count

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,3 +1,13 @@
-export function AboutPage() {
-  return <div>About</div>;
+import { ReactElement } from "react";
+import { useTodosContext } from "../hooks";
+
+export function AboutPage(): ReactElement {
+  const { todoArray } = useTodosContext();
+
+  return (
+    <div>
+      <div>About</div>
+      <div>Total number of todos: {todoArray.length}</div>
+    </div>
+  );
 }
