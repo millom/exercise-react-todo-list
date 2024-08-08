024933db31ab267f731054812064cb72028c4326
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-08-08 18:06:02 +0200

    Header: Fix buttons for sorting and remove selected

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,12 @@ import { SortType } from "../enums";
 interface IHeaderProps {
   removeTodoFunc: () => void;
   sortListFunc: (type: SortType) => void;
-  sortType: SortType;
-  updateSortType: (sortType: SortType) => void;
   showRemoveButton: boolean;
 }
 
 export function Header({
   removeTodoFunc,
   sortListFunc,
-  sortType,
-  updateSortType,
   showRemoveButton,
 }: IHeaderProps): ReactElement {
   return (
@@ -23,12 +19,23 @@ export function Header({
         <h1 className="header-text">My todo list</h1>
 
         <div className="header-navbar">
+          <button
+            className={showRemoveButton ? "btn" : "btn hidden"}
+            type="button"
+            // disabled={showRemoveButton ? undefined : true}
+            onClick={removeTodoFunc}
+          >
+            Remove selected
+          </button>
+
           <Link to="/" className="link">
             Home
           </Link>
+
           <Link to="/add-todo" className="link">
             Add todo
           </Link>
+
           <Link to="/about" className="link">
             About
           </Link>
@@ -37,8 +44,20 @@ export function Header({
 
       <div className="header-sort-container">
         <h3 className="header-sort-text">Sort order</h3>
-        <div className="header-sort-checkbox-container">
-          <div className="header-sort-checkbox-container">
+        <div className="header-sort-btn-container">
+          <button
+            className="sm-btn"
+            onClick={() => sortListFunc(SortType.Timestamp)}
+          >
+            Timestamp
+          </button>
+          <button
+            className="sm-btn"
+            onClick={() => sortListFunc(SortType.Author)}
+          >
+            Author
+          </button>
+          {/* <div className="header-sort-checkbox-container">
             <input
               id="custom"
               type="radio"
@@ -69,21 +88,21 @@ export function Header({
               onChange={() => updateSortType(SortType.Author)}
             />
             <label htmlFor="author">Author</label>
-          </div>
+          </div> */}
         </div>
-        <button className="sm-btn" onClick={() => sortListFunc(sortType)}>
+        {/* <button className="sm-btn" onClick={() => sortListFunc(sortType)}>
           Sort
-        </button>
+        </button> */}
       </div>
 
-      <button
+      {/* <button
         className={showRemoveButton ? "btn" : "btn hidden"}
         type="button"
         // disabled={showRemoveButton ? undefined : true}
         onClick={removeTodoFunc}
       >
         Remove selected
-      </button>
+      </button> */}
     </div>
   );
 }
